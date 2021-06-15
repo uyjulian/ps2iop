4e56f213f4f5a645481520378b1e2c13ffc7fe1d
uyjulian <uyjulian@gmail.com>
2021-06-14 23:59:58 -0500

    Initial adaptation of cdvdfsv for ps2iop

--- iop/cdvdfsv/irx_imports.h
+++ iop/cdvdfsv/irx_imports.h
@@ -1,16 +1,18 @@
-
 #ifndef IOP_IRX_IMPORTS_H
 #define IOP_IRX_IMPORTS_H
 
-#include "irx.h"
+#include <irx.h>
 
-#include "sysmem.h"
-#include "loadcore.h"
-#include "intrman.h"
-#include "stdio.h"
-#include "thbase.h"
-#include "sifcmd.h"
-#include "sifman.h"
-#include "cdvdman.h"
+#include <cdvdman.h>
+#include <intrman.h>
+#include <loadcore.h>
+#include <sifcmd.h>
+#include <sifman.h>
+#include <thbase.h>
+#include <thevent.h>
+#include <thsemap.h>
+#include <sysclib.h>
+#include <sysmem.h>
+#include <stdio.h>
 
-#endif /* IOP_IRX_IMPORTS_H */
+#endif
