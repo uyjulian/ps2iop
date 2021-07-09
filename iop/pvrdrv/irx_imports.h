3480231643a59982ac650a610ac70640184736b4
uyjulian <uyjulian@gmail.com>
2021-07-09 13:29:27 -0500

    Initial compiling dvrdrv

---
+++ iop/pvrdrv/irx_imports.h
@@ -0,0 +1,15 @@
+
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include "irx.h"
+
+#include "dev9.h"
+#include "sysmem.h"
+#include "intrman.h"
+#include "loadcore.h"
+#include "stdio.h"
+#include "thbase.h"
+#include "thsemap.h"
+
+#endif /* IOP_IRX_IMPORTS_H */
