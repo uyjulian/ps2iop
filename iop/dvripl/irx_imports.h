092d925cb7b6e5ea3ae0315a195d809f1947eba0
uyjulian <uyjulian@gmail.com>
2021-08-09 02:43:43 -0500

    The rest of the PVR drivers

---
+++ iop/dvripl/irx_imports.h
@@ -0,0 +1,15 @@
+
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include "irx.h"
+
+#include "iomanX.h"
+#include "pvrdrv.h"
+#include "stdio.h"
+#include "thbase.h"
+#include "thsemap.h"
+#include "timrman.h"
+#include "sysmem.h"
+
+#endif /* IOP_IRX_IMPORTS_H */
---
+++ iop/dvrmisc/irx_imports.h
@@ -0,0 +1,13 @@
+
+#ifndef IOP_IRX_IMPORTS_H
+#define IOP_IRX_IMPORTS_H
+
+#include "irx.h"
+
+#include "iomanX.h"
+#include "pvrdrv.h"
+#include "stdio.h"
+#include "thbase.h"
+#include "thsemap.h"
+
+#endif /* IOP_IRX_IMPORTS_H */
