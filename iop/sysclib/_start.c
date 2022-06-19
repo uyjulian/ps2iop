1b172c88d521a941a6ce31b3e1b705b298eed8ca
uyjulian <uyjulian@gmail.com>
2022-06-19 12:46:30 -0500

    sysclib: handle unused arguments

--- iop/sysclib/_start.c
+++ iop/sysclib/_start.c
@@ -8,6 +8,9 @@ extern struct irx_export_table _exp_sysclib;
 extern struct irx_export_table _exp_stdio;
 
 int _start(int argc, char* argv[]) {
+	(void)argc;
+	(void)argv;
+
     if (RegisterLibraryEntries(&_exp_sysclib) != 0)
         return 1;
     if (RegisterLibraryEntries(&_exp_stdio) != 0)
