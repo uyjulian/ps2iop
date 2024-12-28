3d68ce72a6b848fa41991fc5697072bae1e476a4
Julian Uy <uyjulian@gmail.com>
2024-12-28 12:39:43 -0600

    a

--- iop/eeconf/irx_imports.h
+++ iop/eeconf/irx_imports.h
@@ -2,9 +2,10 @@
 #ifndef IOP_IRX_IMPORTS_H
 #define IOP_IRX_IMPORTS_H
 
-#include "irx.h"
+#include <irx.h>
 
-#include "sysmem.h"
-#include "loadcore.h"
+#include <loadcore.h>
+#include <sysclib.h>
+#include <sysmem.h>
 
 #endif /* IOP_IRX_IMPORTS_H */
